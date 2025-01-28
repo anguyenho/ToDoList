dfee5a22558667616c7262a01d76101be6caa8a6
Ainsley Nguyenho <anguyenho@lcs.on.ca>
2025-01-28 15:58:48 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ainsley Quyen Nguyenho on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
